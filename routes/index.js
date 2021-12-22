01e6cd5cb7abbf731c25d3d9bd6ceb7251bdea2a
wellpinho <wellpinho@outlook.com>
2021-12-22 20:03:31 -0300

    ✨ NEW FEATURE: delete notes

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,11 @@ const db = require('./../config/db')
 
 const routes = Router()
 
-routes.get('/', (req, res) => {
-  res.render('home')
+routes.get('/', async (req, res) => {
+  const notes = await db.getDb().db().collection('notes').find({}).toArray()
+  res.render('home', {
+    notes
+  })
 })
 
 routes.post('/create', (req, res) => {
@@ -24,11 +27,32 @@ routes.post('/create', (req, res) => {
       description
     })
 
-  return res.redirect(201, '/')
+  res.redirect(301, '/')
 })
 
 routes.get('/create', (req, res) => {
   res.render('create')
+})
+
+routes.post('/delete', async (req, res) => {
+  const data = req.body
+  const id = new ObjectId(data.id)
+
+  const noteExists = await db.getDb().db().collection('notes').findOne({
+    _id: id
+  })
+
+  if (!noteExists) {
+    return res.status(400).json({
+      message: 'Notes not found!'
+    })
+  }
+
+  await db.getDb().db().collection('notes').deleteOne({
+    _id: id
+  })
+
+  res.redirect(301, '/')
 })
 
 module.exports = routes
